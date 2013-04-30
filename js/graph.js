6f81d0e69da9f68a7196ea6621314ffb77a4bec1
Robert Helmer <rhelmer@rhelmer.org>
2013-04-30 16:58:55 -0700

    fancier example

--- js/graph.js
+++ js/graph.js
@@ -1,15 +1,56 @@
 $(function() {
-    var graph = new Rickshaw.Graph( {
-    	element: document.querySelector("#chart"),
-    	width: 235,
-    	height: 85,
-    	renderer: 'scatterplot',
-    	stroke: true,
-    	series: [ {
-    		data: [ { x: 0, y: 40 }, { x: 1, y: 49 }, { x: 2, y: 38 }, { x: 3, y: 30 }, { x: 4, y: 32 } ],
-    		color: 'steelblue'
-    	} ]
-    } );
-    var axes = new Rickshaw.Graph.Axis.Time( { graph: graph } );
-    graph.render();
+// set up our data series with 50 random data points
+
+var seriesData = [ [], [], [] ];
+var random = new Rickshaw.Fixtures.RandomData(150);
+
+for (var i = 0; i < 150; i++) {
+    random.addData(seriesData);
+}
+
+// instantiate our graph!
+
+var graph = new Rickshaw.Graph( {
+    element: document.getElementById("chart"),
+    width: 960,
+    height: 500,
+    renderer: 'line',
+    series: [
+        {
+            color: "#c05020",
+            data: seriesData[0],
+            name: 'New York'
+        }, {
+            color: "#30c020",
+            data: seriesData[1],
+            name: 'London'
+        }, {
+            color: "#6060c0",
+            data: seriesData[2],
+            name: 'Tokyo'
+        }
+    ]
+} );
+
+graph.render();
+
+var hoverDetail = new Rickshaw.Graph.HoverDetail( {
+    graph: graph
+} );
+
+var legend = new Rickshaw.Graph.Legend( {
+    graph: graph,
+    element: document.getElementById('legend')
+
+} );
+
+var shelving = new Rickshaw.Graph.Behavior.Series.Toggle( {
+    graph: graph,
+    legend: legend
+} );
+
+var axes = new Rickshaw.Graph.Axis.Time( {
+    graph: graph
+} );
+axes.render();
 });
