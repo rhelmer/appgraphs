de030b2a2ef454a54706727a1c197489729480f3
Robert Helmer <rhelmer@rhelmer.org>
2013-05-01 15:54:34 -0700

    set limits on y axis

--- js/graph.js
+++ js/graph.js
@@ -5,27 +5,36 @@ $(function() {
 
     $.getJSON('data/test_values.json', function(data) {
         // "page_id" is actually the test id
-        var seriesData = [];
+        var series = {data: [], max: undefined, min: undefined};
+
         $.each(data, function(index, value) {
             if (value.page_id == page_id) {
-                seriesData.push({x: value.date_run, y: value.avg});
+                if (value.avg > series.max || series.max == undefined) {
+                    series.max = value.avg;
+                }
+                if (value.avg < series.min || series.min == undefined) {
+                    series.min = value.avg;
+                }
+                series.data.push({x: value.date_run, y: value.avg});
             }
         });
 
-        graph(seriesData);
+        graph(series);
     });
 });
 
-function graph(seriesData) {
+function graph(series) {
     var graph = new Rickshaw.Graph({
         element: document.getElementById('chart'),
         renderer: 'scatterplot',
-        height: 300,
+        height: 400,
         width: 800,
+        min: series.min - 10,
+        max: series.max + 10,
         series: [
             {
                 color: '#c05020',
-                data: seriesData,
+                data: series.data,
                 name: 'Browser'
             }
         ]
@@ -35,14 +44,14 @@ function graph(seriesData) {
         graph: graph
     });
 
-    var x_axis = new Rickshaw.Graph.Axis.Time( { graph: graph } );
+    var x_axis = new Rickshaw.Graph.Axis.Time({ graph: graph });
 
-    var y_axis = new Rickshaw.Graph.Axis.Y( {
+    var y_axis = new Rickshaw.Graph.Axis.Y({
         graph: graph,
         orientation: 'left',
         tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
         element: document.getElementById('y_axis')
-    } );
+    });
 
     graph.render();
 }
