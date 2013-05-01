3c171e3f078e83ae41f69da3256c56319ef4ff9a
Robert Helmer <rhelmer@rhelmer.org>
2013-04-30 18:59:09 -0700

    quick first pass

--- js/graph.js
+++ js/graph.js
@@ -1,56 +1,75 @@
 $(function() {
-// set up our data series with 50 random data points
 
-var seriesData = [ [], [], [] ];
-var random = new Rickshaw.Fixtures.RandomData(150);
+    $.getJSON('data/test_values.json', function(data) {
+        var by_revision = [];
+        $.each(data, function(index, value) {
+            if (value.revision in by_revision) {
+                by_revision[value.revision].y.push(value.avg);
+            } else {
+                by_revision[value.revision] = {x: value.date_run,
+                                               y: [value.avg]};
+            }
+        });
 
-for (var i = 0; i < 150; i++) {
-    random.addData(seriesData);
+        var seriesData = [];
+        for (var revision in by_revision) {
+            var value = by_revision[revision];
+            console.log(revision);
+            console.log(value);
+            seriesData.push({x: value.x, y: median(value.y)});
+        }
+
+        graph(seriesData);
+    });
+});
+
+function graph(seriesData) {
+    var graph = new Rickshaw.Graph( {
+        element: document.getElementById("chart"),
+        width: 960,
+        height: 500,
+        renderer: 'line',
+        series: [
+            {
+                color: "#c05020",
+                data: seriesData,
+                name: 'Browser'
+            }
+        ]
+    } );
+    
+    graph.render();
+    
+    var hoverDetail = new Rickshaw.Graph.HoverDetail( {
+        graph: graph
+    } );
+    
+    var legend = new Rickshaw.Graph.Legend( {
+        graph: graph,
+        element: document.getElementById('legend')
+    
+    } );
+    
+    var shelving = new Rickshaw.Graph.Behavior.Series.Toggle( {
+        graph: graph,
+        legend: legend
+    } );
+    
+    var axes = new Rickshaw.Graph.Axis.Time( {
+        graph: graph
+    } );
+
+    axes.render();
 }
 
-// instantiate our graph!
-
-var graph = new Rickshaw.Graph( {
-    element: document.getElementById("chart"),
-    width: 960,
-    height: 500,
-    renderer: 'line',
-    series: [
-        {
-            color: "#c05020",
-            data: seriesData[0],
-            name: 'New York'
-        }, {
-            color: "#30c020",
-            data: seriesData[1],
-            name: 'London'
-        }, {
-            color: "#6060c0",
-            data: seriesData[2],
-            name: 'Tokyo'
-        }
-    ]
-} );
-
-graph.render();
-
-var hoverDetail = new Rickshaw.Graph.HoverDetail( {
-    graph: graph
-} );
-
-var legend = new Rickshaw.Graph.Legend( {
-    graph: graph,
-    element: document.getElementById('legend')
-
-} );
-
-var shelving = new Rickshaw.Graph.Behavior.Series.Toggle( {
-    graph: graph,
-    legend: legend
-} );
-
-var axes = new Rickshaw.Graph.Axis.Time( {
-    graph: graph
-} );
-axes.render();
-});
+function median(values) {
+    values.sort( function(a,b) {return a - b;} );
+  
+    var half = Math.floor(values.length/2);
+  
+    if(values.length % 2) {
+        return values[half];
+    } else {
+        return (values[half-1] + values[half]) / 2.0;
+    }
+}
