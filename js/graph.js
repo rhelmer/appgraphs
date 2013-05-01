99b37a0c43ddc8ceeaff1e2e54922b0189279dcf
Robert Helmer <rhelmer@rhelmer.org>
2013-05-01 16:09:17 -0700

    remove tabs, and start prototyping warning on last run being too long

--- js/graph.js
+++ js/graph.js
@@ -1,11 +1,13 @@
 $(function() {
 
     // FIXME get this from datazilla pages JSON, 34 is "cold_load_time" test
-    var page_id = 34;
+    var page_id = 34,
+            max_time = 800;
 
     $.getJSON('data/test_values.json', function(data) {
         // "page_id" is actually the test id
-        var series = {data: [], max: undefined, min: undefined};
+        var series = {data: [], max: undefined, min: undefined, 
+                      last_date: undefined, last_value: undefined};
 
         $.each(data, function(index, value) {
             if (value.page_id == page_id) {
@@ -15,9 +17,22 @@ $(function() {
                 if (value.avg < series.min || series.min == undefined) {
                     series.min = value.avg;
                 }
+                if (value.date_run > series.last_date ||
+                    series.last_date == undefined) {
+                    series.last_date = value.date_run;
+                    series.last_value = value.avg;
+                }
+
                 series.data.push({x: value.date_run, y: value.avg});
             }
         });
+
+        console.log(series.last_value);
+        console.log(series.last_date);
+
+        if (series.last_value >= max_time) {
+            alert('warning - last test run took longer than ' + max_time +'ms');
+        }
 
         graph(series);
     });
